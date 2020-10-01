27b646ab6b69b6a92862b4cefa79847c56f1ba13
SalehMajeed <saleh.majeed8@gmail.com>
2020-10-01 17:26:40 -0400

    prototypes

--- method.js
+++ method.js
@@ -1,48 +1,48 @@
-// let obj = {
-//     a: 2,
-//     b: function(){
-//         return this.a + 5
-//     }
-// }
+let obj = {
+    a: 2,
+    b: function(){
+        return this.a + 5
+    }
+}
 
-// console.log(obj.a) // 2
-// console.log(obj.b()) // 7
+console.log(obj.a) // 2
+console.log(obj.b()) // 7
 
-// console.log('\n')
+console.log('\n')
 
-// let inherited = Object.create(obj)
+let inherited = Object.create(obj)
 
-// console.log(inherited.a) // 2
-// console.log(inherited.b()) // 7
+console.log(inherited.a) // 2
+console.log(inherited.b()) // 7
 
-// console.log('\n')
+console.log('\n')
 
-// inherited.a = function(){return this.b + 10}
-// inherited.b = 10;
+inherited.a = function(){return this.b + 10}
+inherited.b = 10;
 
-// console.log(inherited.a()) // 20
-// console.log(inherited.b) // 10
+console.log(inherited.a()) // 20
+console.log(inherited.b) // 10
 
-// function method(a, b) {
-//     this.a = a;
-//     this.b = b;
-// }
+function method(a, b) {
+    this.a = a;
+    this.b = b;
+}
 
-// method.prototype.print = function () {
-//     let obj = {
-//         prints: () => {
-//             return [this.a, this.b, 'hi'];
-//         },
-//     };
-//     return obj;
-// };
+method.prototype.print = function () {
+    let obj = {
+        prints: () => {
+            return [this.a, this.b, 'hi'];
+        },
+    };
+    return obj;
+};
 
-// var call = new method(10, 20);
+var call = new method(10, 20);
 
-// console.log(call.print().prints()) // [10, 20, 'hi']
+console.log(call.print().prints()) // [10, 20, 'hi']
 
 function parent() {}
-parent.prototype.val = 20
+parent.prototype.val = 20 // known as monkey patching and is bad practice.
 parent.val = 10
 parent.__proto__.value = 30
 var x = new parent()
@@ -51,3 +51,5 @@ console.log(x)
 console.log(parent.val) //10
 console.log(parent.prototype.val) // 20
 console.log(parent.__proto__.value) // 30
+
+// hard to know what happening use hasOwnProperty that give result by precise checking from given location.
